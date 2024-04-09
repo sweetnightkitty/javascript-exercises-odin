d4608648edf14cb904bbde3fb0e814010b22bf7b
SaraToth <saratoth101@gmail.com>
2024-04-09 20:30:23 +0900

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(small, large) {
+    let sum = 0;
+    if(typeof small !== 'number' || typeof large !== 'number') return "ERROR";
+    if(small < 0 || large < 0) return "ERROR";
+    if(small > large) {
+        let temp = large;
+        large = small;
+        small = temp;
+    }
 
+    for(let i = small; i <= large; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
