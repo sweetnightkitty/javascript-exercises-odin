e4824975c7983684c307be1fa4e6ef6383130622
SaraToth <saratoth101@gmail.com>
2024-04-09 18:04:57 +0900

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+const array = []
+
+if(num >= 0) {
+    for(i = 0; i < num; i++) {
+        array.push(string);
+    }
+    return array.join("");    
+} else {
+    return "ERROR";
+}
 
 };
 
