08c0af0ab1fad08e09db7ebd5bc9bce021aa5909
SaraToth <saratoth101@gmail.com>
2024-04-09 18:28:33 +0900

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let forwardArray = string.split(""); //s,t,r,i,n,g
+    let reverseArray = [];
+    const num = forwardArray.length;
+    for(let i = 0; i < num; i++){
+        reverseArray.push(forwardArray.pop());
+    }
+    return reverseArray.join("");
 };
 
 // Do not edit below this line
