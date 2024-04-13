d101e06e052b8139e7bb93b8a495bfdec39fb799
SaraToth <saratoth101@gmail.com>
2024-04-14 00:51:44 +0900

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+   return people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = calculateAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = calculateAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
 
+    return currentAge > oldestAge ? currentPerson : oldestPerson;
+    });
 };
 
+function calculateAge(birth, death) {
+    if(!death) death = 2024;
+    return death - birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
