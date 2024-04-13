61bf99067e8cb708fa07db1279906d04ee7af8ff
SaraToth <saratoth101@gmail.com>
2024-04-13 22:38:43 +0900

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+const punctuation = /[\.,?! ]/g;
 
+let newString = string.toLowerCase();
+newString = newString.replace(punctuation, "");
+
+let forwards = newString.split("");
+let backwards = [];
+let length = forwards.length;
+
+for(let i = length - 1; i >= 0; i--) {
+    backwards.push(forwards[i]);
 };
+
+return forwards.join() === backwards.join();
+};
+
 
 // Do not edit below this line
 module.exports = palindromes;
