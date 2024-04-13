ba9e9a349c12934ab68c134959d03046fb680907
SaraToth <saratoth101@gmail.com>
2024-04-13 23:02:23 +0900

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+if(position < 0) return "OOPS";
+if(position == 1 || position == 2) return 1;
 
+let fibonacci = [1, 1];
+let secondToLastNumber = 1;
+let lastNumber = 1;
+
+for(let i = 3; i <= position; i++) {
+    currentNumber = secondToLastNumber + lastNumber;
+    fibonacci.push(currentNumber);
+
+    secondToLastNumber = lastNumber;
+    lastNumber = currentNumber;
+};
+
+return fibonacci[position -1];
 };
 
 // Do not edit below this line
