f406cee40d14f2d9ff8971ed0034ae33c77a16ae
SaraToth <saratoth101@gmail.com>
2024-04-13 21:47:20 +0900

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currentItem) => total + currentItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+ return arr.reduce((total, currentItem) => total * currentItem, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	//if num is 5
+  let arr = [];
+
+  if(num === 0) {
+    arr.push(1);
+  } else {
+    for(let i = num; i > 0; i--) {
+      arr.push(i);
+    }
+  }
+
+  return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 };
 
 // Do not edit below this line
